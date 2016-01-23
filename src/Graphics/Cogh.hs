d48b58b91742d8441886763fe9ab6301fd15d876
Ivo Kosir <ivo.kosir93@gmail.com>
2016-01-23 19:00:11 +0100

    Export Render module as public

--- src/Graphics/Cogh.hs
+++ src/Graphics/Cogh.hs
@@ -2,6 +2,7 @@ module Graphics.Cogh
   ( module Graphics.Cogh.Element
   , module Graphics.Cogh.Event
   , module Graphics.Cogh.Image
+  , module Graphics.Cogh.Render
   , module Graphics.Cogh.Text
   , module Graphics.Cogh.Window
   ) where
@@ -9,5 +10,6 @@ module Graphics.Cogh
 import Graphics.Cogh.Element
 import Graphics.Cogh.Event
 import Graphics.Cogh.Image
+import Graphics.Cogh.Render
 import Graphics.Cogh.Text
 import Graphics.Cogh.Window
