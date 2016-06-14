f2b1d4687dcd2808ce24a50456a000a47e8fcc8b
Ivo Kosir <ivo.kosir93@gmail.com>
2016-06-14 22:19:53 +0200

    Functions for vector rotation

--- src/Graphics/Cogh/Vector.hs
+++ src/Graphics/Cogh/Vector.hs
@@ -28,6 +28,19 @@ vectorScale s Point{x, y} = Point (s * x) (s * y)
 vectorAngle :: Vector -> Angle
 vectorAngle Point{x, y} = atan2 x y
 
+setVectorAngle :: Angle -> Vector -> Vector
+setVectorAngle angle v = vectorRotate (angle-oldAngle) v
+ where
+  oldAngle = vectorAngle v
+
+vectorRotate :: Angle -> Vector -> Vector
+vectorRotate angle Point{x, y} = Point { x = x', y = y' }
+ where
+  cosA = cos angle
+  sinA = sin angle
+  x' = x*cosA - y*sinA
+  y' = x*sinA + y*cosA
+
 vectorLength :: Vector -> Float
 vectorLength Point{x, y} = sqrt (x**2 + y**2)
 
